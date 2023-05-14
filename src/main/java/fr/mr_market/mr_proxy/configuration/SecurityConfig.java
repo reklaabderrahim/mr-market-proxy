1d0588845318d5a4ac27e419a5e9fce83b8e3eaa
Rekla Abderrahim <reklaabderrahim@hotmail.com>
2023-05-14 17:57:25 +0200

    before reinstall arch

--- src/main/java/fr/mr_market/mr_proxy/configuration/SecurityConfig.java
+++ src/main/java/fr/mr_market/mr_proxy/configuration/SecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -31,5 +37,19 @@ public class SecurityConfig {
     @Bean
     ReactiveJwtDecoder reactiveJwtDecoder(AppProperties appProperties) {
         return NimbusReactiveJwtDecoder.withPublicKey(appProperties.getRsa().getPublicKey()).build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials( true );
+        config.setAllowedOrigins( List.of( "*" ) );
+        config.setAllowedMethods( List.of( "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD" ) );
+        config.setAllowedHeaders( List.of( "origin", "content-type", "accept", "authorization", "cookie" ) );
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", config );
+
+        return new CorsWebFilter( source );
     }
 }
