62b47d08ea6be5d847e22587f9fdc0b90658bb90
rekla Abderrahim <reklaabderrahim@hotmail.com>
2023-05-29 20:39:47 +0200

    fix dns resolver problem

---
+++ src/main/java/fr/mr_market/mr_proxy/configuration/HttpClientResolverFixConfig.java
@@ -0,0 +1,16 @@
+package fr.mr_market.mr_proxy.configuration;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.cloud.gateway.config.HttpClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpClientResolverFixConfig {
+
+    @Bean
+    public HttpClientCustomizer httpClientResolverCustomizer() {
+        return httpClient -> httpClient.resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+
+}
